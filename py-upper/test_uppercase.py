93b4dcd4c75ca8e982dbe98074198fa80419c3e9
rafal <rafal@somewhere.com>
2025-12-12 13:27:56 +0100

    I have ERRROR

--- py-upper/test_uppercase.py
+++ py-upper/test_uppercase.py
@@ -3,7 +3,7 @@ from uppercase import to_upper
 
 def test_to_uppercase():
     assert to_upper("hello") == "HELLO"
-    assert to_upper("Python") == "PYTHON"
+    assert to_upper("Python") == "PYTHON1"
     assert to_upper("123abc") == "123ABC"
     assert to_upper("") == ""  # Test empty string
 if __name__ == "__main__":
